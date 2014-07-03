87339aa0b930d355902194012f0281d97389683c
viktorwidiker <viktor.widiker@gmail.com>
2014-07-03 11:04:56 +0200

    Refactoring

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -11,4 +11,4 @@ set_include_path($exarSource . PATH_SEPARATOR . get_include_path());
 
 /** Load Exar autoloader and register namespaces */
 require_once __DIR__ . '/Exar/Autoloader.php';
-Exar\Autoloader::register(dirname(dirname(__FILE__)) . '/_cache', array('Exar'));
+Exar\Autoloader::register(dirname(dirname(__FILE__)) . '/_cache', array());
