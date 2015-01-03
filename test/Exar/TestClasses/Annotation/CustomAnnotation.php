bcf54bbac36261d4837f6b141996d56d755e42da
viktorwidiker <viktor.widiker@gmail.com>
2015-01-03 02:50:17 +0100

    Refactoring

--- test/Exar/TestClasses/Annotation/CustomAnnotation.php
+++ test/Exar/TestClasses/Annotation/CustomAnnotation.php
@@ -2,11 +2,6 @@
 namespace Exar\TestClasses\Annotation;
 
 use Exar\Annotation\Annotation;
-use Exar\Aop\Interceptor\Interfaces\AfterReturningInterceptor;
-use Exar\Aop\Interceptor\Interfaces\AfterThrowingInterceptor;
-use Exar\Aop\Interceptor\Interfaces\AfterInvocationInterceptor;
-use Exar\Aop\Interceptor\Interfaces\BeforeInvocationInterceptor;
-use Exar\Aop\InvocationContext;
 
 /**
  * @Target("class")
